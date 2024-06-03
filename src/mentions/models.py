2d62cb05af7305516a9885b14a59bb32e953bcd0
BinDruid <abharya.dev@gmail.com>
2024-06-03 19:51:27 +0330

    [Refactor] Update comments app name to mentions

--- src/mentions/models.py
+++ src/mentions/models.py
@@ -5,12 +5,12 @@ from src.database.core import Base
 from src.database.mixin_models import TimeStampedModel
 
 
-class Comment(Base, TimeStampedModel):
-    __tablename__ = 'comments'
+class Mention(Base, TimeStampedModel):
+    __tablename__ = 'mentions'
 
     id = Column(Integer, primary_key=True)
     author_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
-    author = relationship('User', backref='comments')
+    author = relationship('User', backref='mentions')
     post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'))
-    post = relationship('Post', backref='comments')
+    post = relationship('Post', backref='mentions')
     content = Column(String, nullable=False)

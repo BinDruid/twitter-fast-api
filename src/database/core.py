60816117c63cc885a2b462b72efa28c0601c108a
BinDruid <abharya.dev@gmail.com>
2024-06-04 04:05:05 +0330

    [Update] metadata naming convention

--- src/database/core.py
+++ src/database/core.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi import Depends
 from pydantic import BaseModel
 from pydantic.types import SecretStr
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
@@ -21,7 +21,17 @@ def get_db_session() -> Session:
 
 DbSession = Annotated[Session, Depends(get_db_session)]
 
-Base = declarative_base()
+POSTGRES_INDEXES_NAMING_CONVENTION = {
+    'ix': '%(column_0_label)s_idx',
+    'uq': '%(table_name)s_%(column_0_name)s_key',
+    'ck': '%(table_name)s_%(constraint_name)s_check',
+    'fk': '%(table_name)s_%(column_0_name)s_fkey',
+    'pk': '%(table_name)s_pkey',
+}
+
+metadata = MetaData(naming_convention=POSTGRES_INDEXES_NAMING_CONVENTION)
+
+Base = declarative_base(metadata=metadata)
 
 
 class PydanticBase(BaseModel):

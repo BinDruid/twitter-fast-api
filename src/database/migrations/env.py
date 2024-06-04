9b08ed4ab76836a4695a3acbd85c5782acaad76e
BinDruid <abharya.dev@gmail.com>
2024-06-04 22:38:06 +0330

    [Refactor] Remove mention and move its logic to posts

--- src/database/migrations/env.py
+++ src/database/migrations/env.py
@@ -7,7 +7,6 @@ from alembic import context
 from src.core.config import settings
 from src.users.models import User, Followership
 from src.posts.models import Post
-from src.mentions.models import Mention
 from src.likes.models import Like
 from src.database.core import Base
 # this is the Alembic Config object, which provides
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from pydantic import Field
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 
 from src.core.pagination import Pagination
@@ -16,6 +16,10 @@ class Post(Base, TimeStampedModel):
     content = Column(String(128), nullable=False)
     author_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     author = relationship('User', backref='posts')
+    original_post_id = Column(Integer, ForeignKey('posts.id'), nullable=True)
+    is_quote = Column(Boolean, default=False)
+    is_mention = Column(Boolean, default=False)
+
 
     def __str__(self):
         return f'{self.author}#{self.id}'

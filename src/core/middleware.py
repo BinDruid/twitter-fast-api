b938b57e607aae501c2e68949a000b78dc63d0e5
BinDruid <abharya.dev@gmail.com>
2024-06-04 15:51:06 +0330

    [Feat] Add logging middleware

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -1,10 +1,28 @@
+import datetime
+
 from fastapi import status
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 from pydantic import ValidationError
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
-
+from src.core.config import settings
 from .logging import logger
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        start_time = datetime.datetime.utcnow()
+        method_name = request.method.upper()
+        url = request.url
+        query_params = request.query_params
+        path_params = request.path_params
+        with open(f'{settings.PATHS.ROOT_DIR}/logs/requests.log', mode='a') as request_logs:
+            log_message = f'[{start_time}] [{method_name}] {url} ,query: {query_params}, path: {path_params}\n'
+            request_logs.write(log_message)
+        response = await call_next(request)
+        process_time = datetime.datetime.utcnow() - start_time
+        response.headers['X-Process-Time'] = str(process_time)
+        return response
 
 
 class ExceptionMiddleware(BaseHTTPMiddleware):

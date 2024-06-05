4c8555e9892043a10203a961b9685d105a4e5d76
BinDruid <abharya.dev@gmail.com>
2024-06-05 20:54:30 +0330

    [Refactor] Add engagements as separate module

--- api/database/migrations/env.py
+++ api/database/migrations/env.py
@@ -7,7 +7,7 @@ from alembic import context
 from api.core.config import settings
 from api.users.models import User, Followership
 from api.posts.models import Post, Mention
-from api.likes.models import Like
+from api.engagements.models import Like, Bookmark, View
 from api.database.configs import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

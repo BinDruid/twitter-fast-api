61cafaa115c119ca1f95550c7bf45a22577c1fa6
BinDruid <abharya.dev@gmail.com>
2024-06-05 12:59:39 +0330

    [Fix] Remove direct depends from service layer

--- src/posts/services.py
+++ src/posts/services.py
@@ -3,22 +3,21 @@ from sqlalchemy.orm import Query
 from src.database import DbSession
 from src.users.models import User
 
-from .depends import AuthorByName, PostByAuthor, PostByID
 from .models import Mention, Post, PostPayload
 
 
-def get_post_by_author(*, db_session: DbSession, author: AuthorByName) -> Query[Post]:
+def get_post_by_author(*, db_session: DbSession, author: User) -> Query[Post]:
     author_posts = db_session.query(Post).filter(Post.author_id == author.id)
     return author_posts
 
 
-def get_mentions_by_post(*, db_session: DbSession, post: PostByAuthor) -> Query[Post]:
+def get_mentions_by_post(*, db_session: DbSession, post: Post) -> Query[Post]:
     subquery = db_session.query(Mention.mention_id).filter(Mention.original_post_id == post.id).subquery()
     mentions = db_session.query(Post).filter(Post.id.in_(subquery))
     return mentions
 
 
-def create_mention_for_post(*, db_session: DbSession, author: User, post: PostByAuthor, context: PostPayload) -> Post:
+def create_mention_for_post(*, db_session: DbSession, author: User, post: Post, context: PostPayload) -> Post:
     new_mention = Post(content=context.content, author_id=author.id)
     db_session.add(new_mention)
     db_session.commit()
@@ -28,13 +27,13 @@ def create_mention_for_post(*, db_session: DbSession, author: User, post: PostBy
     return new_mention
 
 
-def get_quotes_by_post(*, db_session: DbSession, post: PostByAuthor) -> Query[Post]:
+def get_quotes_by_post(*, db_session: DbSession, post: Post) -> Query[Post]:
     subquery = db_session.query(Post.id).filter(Post.quoted_post_id == post.id).subquery()
     quotes = db_session.query(Post).filter(Post.id.in_(subquery))
     return quotes
 
 
-def create_quote_for_post(*, db_session: DbSession, author: User, post: PostByAuthor, context: PostPayload) -> Post:
+def create_quote_for_post(*, db_session: DbSession, author: User, post: Post, context: PostPayload) -> Post:
     new_quote = Post(content=context.content, author_id=author.id, quoted_post_id=post.id)
     db_session.add(new_quote)
     db_session.commit()
@@ -48,12 +47,12 @@ def create_post_for_user(*, db_session: DbSession, author: User, context: PostPa
     return new_post
 
 
-def update_post_for_user(*, db_session: DbSession, post: PostByID, context: PostPayload) -> Post:
+def update_post_for_user(*, db_session: DbSession, post: Post, context: PostPayload) -> Post:
     post.content = context.content
     db_session.commit()
     return post
 
 
-def delete_post_for_user(*, db_session: DbSession, post: PostByID) -> None:
+def delete_post_for_user(*, db_session: DbSession, post: Post) -> None:
     db_session.delete(post)
     db_session.commit()

98314e97b1e6934cb20f1cc83b11056d9e0dce7b
BinDruid <abharya.dev@gmail.com>
2024-06-05 15:40:11 +0330

    [Refactor] Add user depends and reorganize user routes template

--- src/users/depends.py
+++ src/users/depends.py
@@ -4,14 +4,54 @@ from fastapi import Depends, HTTPException
 
 from src.database.core import DbSession
 
-from .models import User
+from .models import Followership, User
 
 
-async def get_user_by_name(username: str, db_session: DbSession) -> User:
-    author = db_session.query(User).filter(User.username == username).one_or_none()
-    if author is None:
+def get_user_by_name(username: str, db_session: DbSession) -> User:
+    user = db_session.query(User).filter(User.username == username).one_or_none()
+    if user is None:
         raise HTTPException(status_code=404, detail=f'User {username} not found')
-    return author
+    return user
 
 
 UserByName = Annotated[User, Depends(get_user_by_name)]
+
+
+def get_user_by_id(user_id: int, db_session: DbSession) -> User:
+    user = db_session.query(User).filter(User.id == user_id).one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail=f'User {user_id} not found')
+    return user
+
+
+UserByID = Annotated[User, Depends(get_user_by_id)]
+
+
+def get_follower_by_follower_id_and_user(follower_user_id: int, user: UserByID, db_session: DbSession) -> Followership:
+    follower = (
+        db_session.query(Followership)
+        .filter(Followership.follower_id == follower_user_id, Followership.following_id == user.id)
+        .one_or_none()
+    )
+    if not follower:
+        raise HTTPException(status_code=404, detail=f'User #{follower_user_id} does not follow user #{user.id}')
+    return follower
+
+
+FollowerByID = Annotated[Followership, Depends(get_follower_by_follower_id_and_user)]
+
+
+def get_following_by_following_user_id_and_user(
+    following_user_id: int, user: UserByID, db_session: DbSession
+) -> Followership:
+    following = (
+        db_session.query(Followership)
+        .filter(Followership.following_id == following_user_id, Followership.follower_id == user.id)
+        .one_or_none()
+    )
+    if not following:
+        raise HTTPException(status_code=404, detail=f'User #{user.id} does not follow user #{following_user_id}')
+    return following
+
+
+FollowingByID = Annotated[Followership, Depends(get_following_by_following_user_id_and_user)]
--- src/users/views.py
+++ src/users/views.py
@@ -5,82 +5,73 @@ from src.core.pagination import paginate
 from src.database.core import DbSession
 
 from .auth import CurrentUser, InvalidCredentialException
+from .depends import FollowerByID, FollowingByID, UserByID
 from .models import Followership, FollowerShipPayload, User, UserCreatePayload, UserDetail, UserList, UserLoginPayload
 
 auth_router = APIRouter()
 user_router = APIRouter()
 
 
-@user_router.get('/me/', response_model=UserDetail)
-async def get_user_profile(user: CurrentUser, db_session: DbSession):
-    user_profile = db_session.query(User).filter(User.id == user.id).one_or_none()
-    if not user_profile:
-        raise HTTPException(status_code=404, detail=f'User {user.username} not found')
-    return user_profile
+@user_router.get('/profile/{user_id}/', response_model=UserDetail)
+def get_user_profile(db_session: DbSession, user: UserByID):
+    return user
 
 
-@user_router.delete('/me/', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user: CurrentUser, db_session: DbSession):
-    user_profile = db_session.query(User).filter(User.id == user.id).one_or_none()
-    if not user_profile:
-        raise HTTPException(status_code=404, detail=f'User {user.username} not found')
-    db_session.delete(user_profile)
+@user_router.delete('/profile/{user_id}/', status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(current_user: CurrentUser, db_session: DbSession, user: UserByID):
+    if current_user.id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    db_session.delete(user)
     db_session.commit()
     return {'message': f'Deleted user {user.username}'}
 
 
-@user_router.get('/me/followers/', response_model=UserList)
-async def get_user_followers(user: CurrentUser, db_session: DbSession, page: int = 1):
+@user_router.get('/{user_id}/followers/', response_model=UserList)
+def get_user_followers(db_session: DbSession, user: UserByID, page: int = 1):
     subquery = db_session.query(Followership.follower_id).filter(Followership.following_id == user.id).subquery()
     followers = db_session.query(User).filter(User.id.in_(subquery))
     return paginate(items=followers, page=page)
 
 
-@user_router.delete('/me/followers/{follower_id}/', status_code=status.HTTP_204_NO_CONTENT)
-async def remove_user_from_followers(user: CurrentUser, db_session: DbSession, follower_id: int):
-    follower = (
-        db_session.query(Followership)
-        .filter(Followership.follower_id == follower_id, Followership.following_id == user.id)
-        .one_or_none()
-    )
-    if not follower:
-        raise HTTPException(status_code=404, detail=f'User #{follower_id} not found')
+@user_router.delete('/{user_id}/followers/{follower_user_id}/', status_code=status.HTTP_204_NO_CONTENT)
+def remove_user_from_followers(
+    current_user: CurrentUser, db_session: DbSession, user: UserByID, follower: FollowerByID
+):
+    if current_user.id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     db_session.delete(follower)
     db_session.commit()
-    return {'message': f'User #{follower_id} removed from followers'}
+    return {'message': f'User #{follower.follower_id} removed from followers'}
 
 
-@user_router.get('/me/followings/', response_model=UserList)
-def get_user_followings(user: CurrentUser, db_session: DbSession, page: int = 1):
+@user_router.get('/{user_id}/followings/', response_model=UserList)
+def get_user_followings(db_session: DbSession, user: UserByID, page: int = 1):
     subquery = db_session.query(Followership.following_id).filter(Followership.follower_id == user.id).subquery()
     followings = db_session.query(User).filter(User.id.in_(subquery))
     return paginate(items=followings, page=page)
 
 
-@user_router.post('/me/followings/', status_code=status.HTTP_201_CREATED)
-async def follow_user(user: CurrentUser, db_session: DbSession, following: FollowerShipPayload):
+@user_router.post('/{user_id}/followings/', status_code=status.HTTP_201_CREATED)
+def follow_user(current_user: CurrentUser, db_session: DbSession, user: UserByID, following: FollowerShipPayload):
+    if current_user.id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     followership = Followership(follower_id=user.id, following_id=following.user_id)
     db_session.add(followership)
     db_session.commit()
-    return {'message': f'user #{user.id} now is following user #{following.user_id}'}
+    return {'message': f'User #{user.id} now is following user #{following.user_id}'}
 
 
-@user_router.delete('/me/followings/{following_id}/', status_code=status.HTTP_204_NO_CONTENT)
-async def unfollow_user(user: CurrentUser, db_session: DbSession, following_id: int):
-    following = (
-        db_session.query(Followership)
-        .filter(Followership.following_id == following_id, Followership.follower_id == user.id)
-        .one_or_none()
-    )
-    if not following:
-        raise HTTPException(status_code=404, detail=f'User #{following_id} not found')
+@user_router.delete('/{user_id}/followings/{following_user_id}/', status_code=status.HTTP_204_NO_CONTENT)
+def unfollow_user(current_user: CurrentUser, db_session: DbSession, user: UserByID, following: FollowingByID):
+    if user.id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     db_session.delete(following)
     db_session.commit()
-    return {'message': f'Unfollowed user #{following_id}'}
+    return {'message': f'Unfollowed user #{following.following_id}'}
 
 
 @auth_router.post('/', response_model=UserDetail, status_code=status.HTTP_201_CREATED)
-async def create_user(db_session: DbSession, payload: UserCreatePayload):
+def create_user(db_session: DbSession, payload: UserCreatePayload):
     user = User(email=payload.email, username=payload.username, password=payload.password)
     db_session.add(user)
     db_session.commit()
@@ -88,7 +79,7 @@ async def create_user(db_session: DbSession, payload: UserCreatePayload):
 
 
 @auth_router.post('/login/')
-async def login_user(payload: UserLoginPayload, db_session: DbSession):
+def login_user(db_session: DbSession, payload: UserLoginPayload):
     user = db_session.query(User).filter(User.username == payload.username).one_or_none()
     if user is None:
         return InvalidCredentialException

31be0d20616be964f4c9f210754ae96324f57b2d
BinDruid <abharya.dev@gmail.com>
2024-06-07 18:52:28 +0330

    [Chore] Update logging messages

--- twitter_api/engagements/tasks.py
+++ twitter_api/engagements/tasks.py
@@ -7,21 +7,21 @@ from .grpc import post_views_pb2, post_views_pb2_grpc
 
 
 def create_post_view_count_entry(post_id: int):
-    logger.info('Task to increase post #{} views has been called'.format(post_id))
+    logger.info('Task to create post #{} views entry has been called'.format(post_id))
     with grpc.insecure_channel(settings.ANALYTICS_URL) as channel:
         stub = post_views_pb2_grpc.PostViewAnalyticsStub(channel)
         stub.CreateViewCount(post_views_pb2.PostViewRequest(post_id=post_id))
 
 
 def increase_post_view_count(post_id: int):
-    logger.info('Task to increase post #{} views has been called'.format(post_id))
+    logger.info('Task to increase post #{} views entry has been called'.format(post_id))
     with grpc.insecure_channel(settings.ANALYTICS_URL) as channel:
         stub = post_views_pb2_grpc.PostViewAnalyticsStub(channel)
         stub.UpdateViewCount(post_views_pb2.PostViewRequest(post_id=post_id))
 
 
 def delete_post_view_count_entry(post_id: int):
-    logger.info('Task to increase post #{} views has been called'.format(post_id))
+    logger.info('Task to delete post #{} views entry has been called'.format(post_id))
     with grpc.insecure_channel(settings.ANALYTICS_URL) as channel:
         stub = post_views_pb2_grpc.PostViewAnalyticsStub(channel)
         stub.DeleteViewCount(post_views_pb2.PostViewRequest(post_id=post_id))

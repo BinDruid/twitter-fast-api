1508123c649a2fa8b0945d13fa8623269adc1fb5
BinDruid <abharya.dev@gmail.com>
2024-06-14 22:46:25 +0330

    [Test] Add test if user exits before registration

--- tests/users/test_routes.py
+++ tests/users/test_routes.py
@@ -1,13 +1,14 @@
-import pytest
-from psycopg2 import IntegrityError
+from fastapi import status
 from twitter_api.users.models import User
+
+from tests.factories import UserFactory
 
 
 def test_user_can_register_with_valid_register_info(session, client):
     url = '/auth/'
     payload = {'email': 'a.abharya@gmail.com', 'username': 'druid', 'password': '123456'}
     response = client.post(url, json=payload)
-    assert response.status_code == 201
+    assert response.status_code == status.HTTP_201_CREATED
 
     new_user = session.query(User).filter(User.username == payload['username']).one_or_none()
     assert new_user is not None
@@ -15,10 +16,7 @@ def test_user_can_register_with_valid_register_info(session, client):
 
 def test_user_can_not_register_with_with_duplicate_username(session, client):
     url = '/auth/'
-    payload = {'email': 'a.abharya@gmail.com', 'username': 'druid', 'password': '123456'}
-
-    with pytest.raises(IntegrityError):
-        client.post(url, json=payload)
-
-    new_user = session.query(User).filter(User.username == payload['username']).one_or_none()
-    assert new_user is None
+    payload = {'email': 'duplicated_user@gmail.com', 'username': 'druid', 'password': '123456'}
+    UserFactory(username=payload['username'])
+    response = client.post(url, json=payload)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST

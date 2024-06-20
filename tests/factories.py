5936e4350d6b6eb57b84dc77b576330469c6c572
BinDruid <abharya.dev@gmail.com>
2024-06-20 12:58:05 +0330

    [Chore] Add followership factory

--- tests/factories.py
+++ tests/factories.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from factory import LazyFunction, Sequence
+from factory import LazyFunction, SubFactory
 from factory.alchemy import SQLAlchemyModelFactory
 from factory.fuzzy import FuzzyDateTime
 from faker import Faker
 from faker.providers import misc
 from pytz import UTC
-from twitter_api.users.models import User, hash_password
+from twitter_api.users.models import Followership, User, hash_password
 
 from .configs import Session
 
@@ -28,8 +28,16 @@ class TimeStampBaseFactory(BaseFactory):
 
 class UserFactory(TimeStampBaseFactory, BaseFactory):
     username = fake.user_name()
-    email = Sequence(lambda n: f'user{n}@example.com')
-    password = LazyFunction(lambda: hash_password('test123').decode('utf-8'))
+    email = fake.email()
+    password = LazyFunction(lambda: hash_password('test123'))
 
     class Meta:
         model = User
+
+
+class FollowershipFactory(TimeStampBaseFactory, BaseFactory):
+    following = SubFactory(UserFactory)
+    follower = SubFactory(UserFactory)
+
+    class Meta:
+        model = Followership

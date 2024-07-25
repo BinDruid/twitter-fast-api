e4ffebe6ddf5aeb555749fe05a5afa6cdd64c443
BinDruid <abharya.dev@gmail.com>
2024-07-25 21:41:43 +0330

    [Feat] Enable CORS headers

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sentry_asgi import SentryMiddleware
 
-from twitter_api.core.config import app_configs
+from twitter_api.core.config import app_configs, settings
 from twitter_api.core.lifespan import lifespan
 from twitter_api.core.middleware import AuthenticationMiddleware, ExceptionMiddleware, LoggingMiddleware
 from twitter_api.routes import api_router
@@ -17,5 +18,13 @@ api = get_application()
 
 api.add_middleware(SentryMiddleware)
 api.add_middleware(LoggingMiddleware)
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_origin_regex=settings.CORS_ORIGINS_REGEX,
+    allow_credentials=True,
+    allow_methods=('GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'),
+    allow_headers=settings.CORS_HEADERS,
+)
 api.add_middleware(AuthenticationMiddleware)
 api.add_middleware(ExceptionMiddleware)
